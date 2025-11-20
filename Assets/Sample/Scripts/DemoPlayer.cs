a1c0fb3faec6e21a9a996a102bd62a7bc5d25671
Kaan Yamanyar <kaan@yamanyar.com>
2025-11-20 22:07:56 +0100

    Refactor DemoPlayer to support multiple FakePropellerItems

--- Assets/Sample/Scripts/DemoPlayer.cs
+++ Assets/Sample/Scripts/DemoPlayer.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class DemoPlayer : MonoBehaviour
 {
     public GameObject fakePool;
-    public FakePropellerItem fakeItemFromPool;
+    public FakePropellerItem fakeItem1FromPool;
+    public FakePropellerItem fakeItem2FromPool;
 
     void Awake()
     {
@@ -12,24 +13,26 @@ public class DemoPlayer : MonoBehaviour
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    async UniTask Start()
     {
-        Loop().Forget();
+        Loop(fakeItem1FromPool).Forget();
+        await UniTask.Delay(500);
+        Loop(fakeItem2FromPool).Forget();
     }
 
 
-    private async UniTask Loop()
+    private async UniTask Loop(FakePropellerItem fakePropellerItem)
     {
         while (true)
         {
             //Simulate get from pool
-            fakeItemFromPool.transform.parent = null;
-            await fakeItemFromPool.PlayCreationClip(destroyCancellationToken);
+            fakePropellerItem.transform.parent = null;
+            await fakePropellerItem.PlayCreationClip(destroyCancellationToken);
 
             await UniTask.Delay(1000);
             //Mimic removal
-            fakeItemFromPool.transform.parent = fakePool.transform;
-            fakeItemFromPool.OnBackToPool();
+            fakePropellerItem.transform.parent = fakePool.transform;
+            fakePropellerItem.OnBackToPool();
             await UniTask.Delay(1000);
         }
     }
